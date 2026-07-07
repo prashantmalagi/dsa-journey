6a6d36c89530f8a86d997e29c9e685fb82ebacfb
Prashant Malagi <prashantmalagi60@gmail.com>
2026-07-07 19:51:34 +0530

    Update print_name function call and enhance fizzBuzz class implementation

--- recursion/rec.py
+++ recursion/rec.py
@@ -4,13 +4,27 @@
     print(n)
     printnumber(n + 1)
 printnumber(1)
-"""
+
 def print_name(name,n,i):
     # n = int(input("enter n value:"))
     if i == n+1:
         return
     print(name)
     print_name(name, n, i + 1)
-print_name("sai", 8, 0)
+print_name("pnm", 8, 0)
 
-    
+"""
+class Solution(object):
+    def fizzBuzz(self, n):
+        ans = []
+        for i in range(1, n + 1):
+            if i % 3 == 0 and i % 5 == 0:
+                ans.append("FizzBuzz")
+            elif i % 3 == 0:
+                ans.append("Fizz")
+            elif i % 5 == 0:
+                ans.append("Buzz")
+            else:
+                ans.append(str(i))
+        return ans
+
