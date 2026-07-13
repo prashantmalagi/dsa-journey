a6ae08baed850c979aeb516d4ae5c82e9802eb4d
Prashanth Malagi <prashantmalagi60@gmail.com>
2026-07-13 20:23:20 +0530

    Time: 0 ms (100%), Space: 12.2 MB (89.09%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        if n<=0:
+            return False
+        elif n==1:
+            return True
+        elif n%2!=0:
+            return False
+        else:
+            return self.isPowerOfTwo(n//2)
+
+        
